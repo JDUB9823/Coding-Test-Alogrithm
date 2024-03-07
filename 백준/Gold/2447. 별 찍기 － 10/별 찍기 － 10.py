1e6f554d81a6629dc9fe8375ebbc59b070c7685b
Jeongwoo Cho <148315449+JDUB9823@users.noreply.github.com>
2024-03-07 16:27:14 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 56 ms, Memory: 40556 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -1,28 +1,17 @@
-def fillblank(board, x, y, size):
-    for i in range(size):
-        for j in range(size):
-            board[x+i][y+j] = " "
+def draw_star(size):
+    if size == 1:
+        return ["*"]
+
+    stars = draw_star(size//3)
+    res = []
+
+    for star in stars:
+        res.append(star*3)
+    for star in stars:
+        res.append(star+" "*(size//3)+star)
+    for star in stars:
+        res.append(star*3)
+    return res
     
 N = int(input())
-#NxN 배열을 모두 *로 채운다
-board = [["*" for _ in range(N)] for _ in range(N)]
-#size x size = 공백을 그려야 하는 크기
-size = N//3
-
-while True:
-    if size < 1:
-        break
-    #x, y는 " "로 바꿀 board의 왼쪽 상단 위치로 초기화
-    x, y = size, size
-
-    #size * 3만큼 indexing을 통해 다음 공백의 위치 지정
-    for i in range(x,N,size*3):
-        for j in range(y,N,size*3):
-            fillblank(board,i,j,size)
-    #해당 size만큼의 공백이 모두 그려지면, 다음 공백의 size로 감소
-    size //= 3
-
-for i in range(N):
-    for j in range(N):
-        print(board[i][j], end="")
-    print()
+print("\n".join(draw_star(N)))
