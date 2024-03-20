41816a191611ab60c4d03075ba5d407510c64f52
Jeongwoo Cho <148315449+JDUB9823@users.noreply.github.com>
2024-03-20 16:12:12 +0900

    Update 쿼드트리.py

--- 백준/Silver/1992. 쿼드트리/쿼드트리.py
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -1,10 +1,8 @@
 def zipVideo(x,y,n):
     global res
-    #왼쪽 상단 색종이의 색
     color = video[x][y]
     for i in range(x,x+n):
         for j in range(y,y+n):
-            #색이 다른 경우
             if video[i][j] != color:
                 color = -1
                 break
