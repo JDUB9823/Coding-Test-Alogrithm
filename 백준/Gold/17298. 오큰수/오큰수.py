5bab75d270cb545c7e07f07a6f9e7ea0ee0bca69
Jeongwoo Cho <148315449+JDUB9823@users.noreply.github.com>
2024-04-01 15:11:19 +0900

    [Gold IV] Title: 오큰수, Time: 564 ms, Memory: 260888 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,11 @@
+n = int(input())
+A = list(map(int, input().split()))
+res = [-1] * n
+stack = [0]
+
+for i in range(1,n):
+    while stack and A[stack[-1]] < A[i]:
+        res[stack.pop()] = A[i]
+    stack.append(i)
+
+print(*res)
