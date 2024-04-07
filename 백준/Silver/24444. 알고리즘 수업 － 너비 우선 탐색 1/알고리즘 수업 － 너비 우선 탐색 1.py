9c010881cc7d8fab9fb63b3c923d347690147eaa
Jeongwoo Cho <148315449+JDUB9823@users.noreply.github.com>
2024-04-07 17:23:04 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 740 ms, Memory: 373372 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque
+sys.setrecursionlimit(10 ** 5)
+input = sys.stdin.readline
+
+def bfs(v):
+    global order
+    visited[v] = order
+    edges[v].sort()
+
+    for vertex in edges[v]:
+        queue.append(vertex)
+
+    while queue:
+        vertex = queue.popleft()
+        if visited[vertex] == 0:
+            order += 1
+            bfs(vertex)
+
+n, m, r = map(int, input().split())
+visited = [0] * (n + 1)
+edges = [[] for _ in range(n + 1)]
+queue = deque()
+order = 1
+
+for _ in range(m):
+    start, end = map(int, input().split())
+    edges[start].append(end)
+    edges[end].append(start)
+
+bfs(r)
+
+for i in range(1, n + 1):
+    print(visited[i])
