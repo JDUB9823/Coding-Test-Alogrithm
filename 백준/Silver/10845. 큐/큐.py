ce50f18ae2a2f4242190d1fefb2cc675903e8579
Jeongwoo Cho <148315449+JDUB9823@users.noreply.github.com>
2024-05-08 10:25:01 +0900

    [Silver IV] Title: 큐, Time: 180 ms, Memory: 115212 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,23 @@
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline())
+q = deque()
+
+for _ in range(N):
+    command = sys.stdin.readline().split()
+    if command[0] == "push":
+        q.appendleft(int(command[1]))
+    elif command[0] == "pop":
+        if q:
+            print(q.pop())
+        else:
+            print(-1)
+    elif command[0] == "size":
+        print(len(q))
+    elif command[0] == "empty":
+        print(1 if not q else 0)
+    elif command[0] == "front":
+        print(q[-1] if q else -1)
+    elif command[0] == "back":
+        print(q[0] if q else -1)
